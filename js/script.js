44ddbdb7374a95c2050b688ee3506b3e7c5eba1b
Dmytro <dima01.com@gmail.com>
2020-10-03 12:35:55 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-let num=5;
+' use strict ';
+
+let numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+};
+
+let lastMov1 = prompt('Какой последний фильм посмотрели?');
+let rateMov1 = prompt('На сколько его оцените?');
+let lastMov2 = prompt('Какой последний фильм посмотрели?');
+let rateMov2 = prompt('На сколько его оцените?');
+
+personalMovieDB.movies[lastMov1] = rateMov1;
+personalMovieDB.movies[lastMov2] = rateMov2;
+    
+console.log(personalMovieDB);
